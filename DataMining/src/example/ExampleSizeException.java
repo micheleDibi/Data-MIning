34adc5da5b93117f826292e586a89bbe9fe890d7
alessia.finiguerra <97960373+alefini@users.noreply.github.com>
2022-06-02 23:14:28 +0200

    Update ExampleSizeException.java

--- DataMining/src/example/ExampleSizeException.java
+++ DataMining/src/example/ExampleSizeException.java
@@ -3,6 +3,8 @@ package example;
 public class ExampleSizeException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
+	
+	public ExampleSizeException() { }
 
 	public ExampleSizeException(String msg) {
 		super(msg);
