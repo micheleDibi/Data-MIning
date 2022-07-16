d9c12a21dda6a4323f103bb1c1ccee7f6629c732
michele dibisceglia <94901756+micheleDibi@users.noreply.github.com>
2022-07-16 16:47:18 +0200

    Delete server directory

--- server/src/database/DatabaseConnectionException.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public class DatabaseConnectionException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	DatabaseConnectionException(String msg){
-		super(msg);
-	}
-}
--- server/src/database/InsufficientColumnNumberException.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public class InsufficientColumnNumberException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public InsufficientColumnNumberException(String msg) {
-		super(msg);
-	}
-}
